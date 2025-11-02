ae6eb48ddfc1e42d9b162565e72d13bf81ce2e5c
shorics <noreplay@shorics.wtf>
2025-11-02 20:55:25 +0100

    remoed api global prefix

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,10 +9,14 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const globalPrefix = '';
+
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+
+  const port = process.env.PORT ?? 3000;
+
   await app.listen(port);
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
