2091dccbd9422f4273ace7da0c5b2b3e2f51d1d5
shorics <noreplay@shorics.wtf>
2025-11-03 23:04:52 +0100

    updated users controller tests

--- apps/api/src/app/users/users.controller.spec.ts
+++ apps/api/src/app/users/users.controller.spec.ts
@@ -1,119 +1,189 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserEdit } from '@pdr-cloud-assessment/shared';
 
+import { NotFoundException } from '@nestjs/common';
+import { EntityNotFoundException } from '../data/data.exceptions';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let serviceMock: jest.MockedObject<UsersService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [{
+        provide: UsersService,
+        useValue: {
+          findAll: jest.fn().mockReturnValue('fake-users-list'),
+          find: jest.fn().mockReturnValue('fake-user-find'),
+          create: jest.fn().mockReturnValue('fake-user-create'),
+          update: jest.fn().mockReturnValue('fake-user-update'),
+          delete: jest.fn(),
+        },
+      }],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    serviceMock = module.get<jest.MockedObject<UsersService>>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-
-  describe('findAll', () => {
+  describe('#findAll', () => {
     it('should return users array', () => {
-      const page = 123;
+      const page = 'fake-page' as unknown as number;
 
       const result = controller.findAll(page);
 
-      expect(result).toEqual([{
-        id: 123,
-        firstName: 'firstName',
-        lastName: 'lastName',
-        email: 'test@email.test',
-        phoneNumber: '213213',
-        birthDate: '2025-12-24',
-        role: 'admin',
-      }]);
+      expect(serviceMock.findAll).toHaveBeenCalledTimes(1);
+      expect(serviceMock.findAll).toHaveBeenCalledWith('fake-page');
+      expect(result).toEqual('fake-users-list');
     });
   });
 
-  describe('find', () => {
+  describe('#find', () => {
     it('should return user', () => {
-      const id = 123;
+      const id = 'fake-id' as unknown as number;
 
       const result = controller.find(id);
 
-      expect(result).toEqual({
-        id: 123,
-        firstName: 'firstName',
-        lastName: 'lastName',
-        email: 'test@email.test',
-        phoneNumber: '213213',
-        birthDate: '2025-12-24',
-        role: 'admin',
+      expect(serviceMock.find).toHaveBeenCalledTimes(1);
+      expect(serviceMock.find).toHaveBeenCalledWith('fake-id');
+      expect(result).toEqual('fake-user-find');
+    });
+
+    describe('with throwing generic exception', () => {
+      beforeEach(() => {
+        serviceMock.find.mockImplementationOnce(() => {
+          throw new Error('test-error');
+        });
+      });
+
+      it('should re-throw error', () => {
+        const id = 'fake-id' as unknown as number;
+
+        const result = () => controller.find(id);
+
+        expect(result).toThrow('test-error');
+      });
+    });
+
+    describe('with throwing entity not found exception', () => {
+      beforeEach(() => {
+        serviceMock.find.mockImplementationOnce(() => {
+          throw new EntityNotFoundException('');
+        });
+      });
+
+      it('should throw not found eception', () => {
+        const id = 'fake-id' as unknown as number;
+
+        const result = () => controller.find(id);
+
+        expect(result).toThrow(NotFoundException);
       });
     });
   });
 
-  describe('create', () => {
+  describe('#create', () => {
     it('should create user', () => {
-      const user: UserEdit = {
-        firstName: 'firstName',
-        lastName: 'lastName',
-        email: 'test@email.test',
-        phoneNumber: '213213',
-        birthDate: '2025-12-24',
-        role: 'admin',
-      };
+      const user = 'fake-user' as unknown as UserEdit;
 
       const result = controller.create(user);
 
-      expect(result).toEqual({
-        id: 101,
-        firstName: 'firstName',
-        lastName: 'lastName',
-        email: 'test@email.test',
-        phoneNumber: '213213',
-        birthDate: '2025-12-24',
-        role: 'admin',
-      });
+      expect(serviceMock.create).toHaveBeenCalledTimes(1);
+      expect(serviceMock.create).toHaveBeenCalledWith('fake-user');
+      expect(result).toEqual('fake-user-create');
     });
   });
 
-  describe('update', () => {
+  describe('#update', () => {
     it('should update user', () => {
-      const id = 123;
-      const user: UserEdit = {
-        firstName: 'firstName',
-        lastName: 'lastName',
-        email: 'test@email.test',
-        phoneNumber: '213213',
-        birthDate: '2025-12-24',
-        role: 'admin',
-      };
+      const id = 'fake-id' as unknown as number;
+      const user = 'fake-user' as unknown as UserEdit;
 
       const result = controller.update(id, user);
 
-      expect(result).toEqual({
-        id: 123,
-        firstName: 'firstName',
-        lastName: 'lastName',
-        email: 'test@email.test',
-        phoneNumber: '213213',
-        birthDate: '2025-12-24',
-        role: 'admin',
+      expect(serviceMock.update).toHaveBeenCalledTimes(1);
+      expect(serviceMock.update).toHaveBeenCalledWith('fake-id', 'fake-user');
+      expect(result).toEqual('fake-user-update');
+    });
+
+    describe('with throwing generic exception', () => {
+      beforeEach(() => {
+        serviceMock.update.mockImplementationOnce(() => {
+          throw new Error('test-error');
+        });
+      });
+
+      it('should re-throw error', () => {
+        const id = 'fake-id' as unknown as number;
+        const user = 'fake-user' as unknown as UserEdit;
+
+        const result = () => controller.update(id, user);
+
+        expect(result).toThrow('test-error');
+      });
+    });
+
+    describe('with throwing entity not found exception', () => {
+      beforeEach(() => {
+        serviceMock.update.mockImplementationOnce(() => {
+          throw new EntityNotFoundException('');
+        });
+      });
+
+      it('should throw not found eception', () => {
+        const id = 'fake-id' as unknown as number;
+        const user = 'fake-user' as unknown as UserEdit;
+
+        const result = () => controller.update(id, user);
+
+        expect(result).toThrow(NotFoundException);
       });
     });
   });
 
-  describe('delete', () => {
+  describe('#delete', () => {
     it('should delete user', () => {
-      const id = 123;
+      const id = 'fake-id' as unknown as number;
 
-      const result = controller.delete(id);
+      controller.delete(id);
 
-      expect(result).toBeUndefined();
+      expect(serviceMock.delete).toHaveBeenCalledTimes(1);
+      expect(serviceMock.delete).toHaveBeenCalledWith('fake-id');
+    });
+
+    describe('with throwing generic exception', () => {
+      beforeEach(() => {
+        serviceMock.delete.mockImplementationOnce(() => {
+          throw new Error('test-error');
+        });
+      });
+
+      it('should re-throw error', () => {
+      const id = 'fake-id' as unknown as number;
+
+        const result = () => controller.delete(id);
+
+        expect(result).toThrow('test-error');
+      });
+    });
+
+    describe('with throwing entity not found exception', () => {
+      beforeEach(() => {
+        serviceMock.delete.mockImplementationOnce(() => {
+          throw new EntityNotFoundException('');
+        });
+      });
+
+      it('should throw not found eception', () => {
+      const id = 'fake-id' as unknown as number;
+
+        const result = () => controller.delete(id);
+
+        expect(result).toThrow(NotFoundException);
+      });
     });
   });
 });
