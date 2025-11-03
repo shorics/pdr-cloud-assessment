cd3a302ceb44242f29247d527f0f66271be9508b
shorics <noreplay@shorics.wtf>
2025-11-03 23:59:00 +0100

    updated e2e happy cases

--- apps/api-e2e/src/api/users.spec.ts
+++ apps/api-e2e/src/api/users.spec.ts
@@ -1,19 +1,32 @@
 import axios from 'axios';
 
 describe('users', () => {
+  beforeAll(async () => {
+      const user = {
+        firstName: 'test-first-name-1',
+        lastName: 'test-last-name-1',
+        email: 'test-1@email.test',
+        phoneNumber: 'test-phone-number-1',
+        birthDate: '2025-12-24',
+        role: 'admin',
+      };
+
+      await axios.post('/users', user);
+  });
+
   describe('GET /users', () => {
     it('should return users array', async () => {
-      const page = 123;
+      const page = 1;
 
       const result = await axios.get('/users', { params: { page } });
 
       expect(result.status).toBe(200);
       expect(result.data).toEqual([{
-        id: 123,
-        firstName: 'firstName',
-        lastName: 'lastName',
-        email: 'test@email.test',
-        phoneNumber: '213213',
+        id: 1,
+        firstName: 'test-first-name-1',
+        lastName: 'test-last-name-1',
+        email: 'test-1@email.test',
+        phoneNumber: 'test-phone-number-1',
         birthDate: '2025-12-24',
         role: 'admin',
       }]);
@@ -22,17 +35,17 @@ describe('users', () => {
 
   describe('GET /users/:id', () => {
     it('should return user', async () => {
-      const id = 123;
+      const id = 1;
 
       const result = await axios.get(`/users/${id}`);
 
       expect(result.status).toBe(200);
       expect(result.data).toEqual({
-        id: 123,
-        firstName: 'firstName',
-        lastName: 'lastName',
-        email: 'test@email.test',
-        phoneNumber: '213213',
+        id: 1,
+        firstName: 'test-first-name-1',
+        lastName: 'test-last-name-1',
+        email: 'test-1@email.test',
+        phoneNumber: 'test-phone-number-1',
         birthDate: '2025-12-24',
         role: 'admin',
       });
@@ -42,59 +55,59 @@ describe('users', () => {
   describe('POST /users', () => {
     it('should create user', async () => {
       const user = {
-        firstName: 'firstName',
-        lastName: 'lastName',
-        email: 'test@email.test',
-        phoneNumber: '213213',
-        birthDate: '2025-12-24',
-        role: 'admin',
+        firstName: 'test-first-name-2',
+        lastName: 'test-last-name-2',
+        email: 'test-2@email.test',
+        phoneNumber: 'test-phone-number-2',
+        birthDate: '2025-01-01',
+        role: 'viewer',
       };
 
       const result = await axios.post('/users', user);
 
       expect(result.status).toBe(201);
       expect(result.data).toEqual({
-        id: 101,
-        firstName: 'firstName',
-        lastName: 'lastName',
-        email: 'test@email.test',
-        phoneNumber: '213213',
-        birthDate: '2025-12-24',
-        role: 'admin',
+        id: 2,
+        firstName: 'test-first-name-2',
+        lastName: 'test-last-name-2',
+        email: 'test-2@email.test',
+        phoneNumber: 'test-phone-number-2',
+        birthDate: '2025-01-01',
+        role: 'viewer',
       });
     });
   });
 
   describe('PUT /users/:id', () => {
-    it('should return user', async () => {
-      const id = 123;
+    it('should update user', async () => {
+      const id = 1;
       const user = {
-        firstName: 'firstName',
-        lastName: 'lastName',
-        email: 'test@email.test',
-        phoneNumber: '213213',
-        birthDate: '2025-12-24',
-        role: 'admin',
+        firstName: 'test-first-name-1-updated',
+        lastName: 'test-last-name-1-updated',
+        email: 'test-1-updated@email.test',
+        phoneNumber: 'test-phone-number-1-updated',
+        birthDate: '2025-06-06',
+        role: 'editor',
       };
 
       const result = await axios.put(`/users/${id}`, user);
 
       expect(result.status).toBe(200);
       expect(result.data).toEqual({
-        id: 123,
-        firstName: 'firstName',
-        lastName: 'lastName',
-        email: 'test@email.test',
-        phoneNumber: '213213',
-        birthDate: '2025-12-24',
-        role: 'admin',
+        id: 1,
+        firstName: 'test-first-name-1-updated',
+        lastName: 'test-last-name-1-updated',
+        email: 'test-1-updated@email.test',
+        phoneNumber: 'test-phone-number-1-updated',
+        birthDate: '2025-06-06',
+        role: 'editor',
       });
     });
   });
 
   describe('DELETE /users/:id', () => {
-    it('should return user', async () => {
-      const id = 123;
+    it('should delete user', async () => {
+      const id = 1;
 
       const result = await axios.delete(`/users/${id}`);
 
