6fe69ee3dc0cbde3be94f75181499a9eb295eb35
shorics <noreplay@shorics.wtf>
2025-11-03 22:51:51 +0100

    set data module to global

--- apps/api/src/app/data/data.module.ts
+++ apps/api/src/app/data/data.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 
 import { DataUsersService } from './data-users.service';
 
+@Global()
 @Module({
   providers: [DataUsersService],
+  exports: [DataUsersService],
 })
 export class DataModule {}
