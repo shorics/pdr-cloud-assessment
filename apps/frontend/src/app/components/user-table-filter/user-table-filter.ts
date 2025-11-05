6ccee74ba574d31e252b2b3b5170926ac92ea9f6
shorics <noreplay@shorics.wtf>
2025-11-05 20:51:59 +0100

    added basic filtering

---
+++ apps/frontend/src/app/components/user-table-filter/user-table-filter.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component, output } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-user-table-filter',
+  templateUrl: './user-table-filter.html',
+  styleUrl: './user-table-filter.scss',
+  imports: [MatFormFieldModule, MatInputModule],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UserTableFilter {
+
+  readonly filterChanged = output<string | undefined>();
+
+  protected onInput(value: string): void {
+    this.filterChanged.emit(value || undefined);
+  }
+}
--- apps/frontend/src/app/views/user-list/user-list.ts
+++ apps/frontend/src/app/views/user-list/user-list.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectionStrategy, Component, effect, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, inject, signal } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { injectDispatch } from '@ngrx/signals/events';
 import { User } from '@pdr-cloud-assessment/shared';
 
 import { UserDetailsDialog, UserDetailsDialogData } from '../../components/user-details-dialog/user-details-dialog';
+import { UserTableFilter } from "../../components/user-table-filter/user-table-filter";
 import { UserTable } from '../../components/user-table/user-table';
 import { userEvents } from '../../state/user.events';
 import { UserStore } from '../../state/user.store';
@@ -12,7 +13,7 @@ import { UserStore } from '../../state/user.store';
   selector: 'app-user-list',
   templateUrl: './user-list.html',
   styleUrl: './user-list.scss',
-  imports: [UserTable],
+  imports: [UserTable, UserTableFilter],
   providers: [UserStore],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,8 +23,12 @@ export class UserList {
   private readonly dispatch = injectDispatch(userEvents);
   private readonly store = inject(UserStore);
 
+  protected filter = signal<string | undefined>(undefined);
   protected user = this.store.user;
   protected userList = this.store.userList;
+  protected userListFiltered = computed(
+    () => this.filterUserList(this.userList().data, this.filter())
+  );
 
   constructor() {
     this.dispatch.loadUserList();
@@ -37,7 +42,25 @@ export class UserList {
     });
   }
 
-  onUserSelect(id: User['id']): void {
+  protected onFilterChange(value: string | undefined): void {
+    this.filter.set(value);
+  }
+
+  protected onUserSelect(id: User['id']): void {
     this.dispatch.loadUser(id);
+  }
+
+  private filterUserList(userList: User[] | undefined, filter: string | undefined): User[] | undefined {
+    if (!filter) {
+      return userList;
+    }
+
+    const filterLower = filter.toLowerCase();
+
+    return userList?.filter((user) => {
+      const fullName = `${user.firstName} ${user.lastName}`;
+
+      return -1 !== fullName.toLowerCase().indexOf(filterLower);
+    });
   }
 }
