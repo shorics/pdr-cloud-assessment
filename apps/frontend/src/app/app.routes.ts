b5f978a68748cac7d75ffc338a1e3d6a376595e3
shorics <noreplay@shorics.wtf>
2025-11-05 18:10:29 +0100

    refactored to component dispatch

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Route } from '@angular/router';
 
-import { userListResolver } from './resolvers/load-user-list.resolver';
 import { UserService } from './services/user.service';
 import { UserListComponent } from './views/user-list/user-list.component';
 
@@ -8,7 +7,6 @@ export const appRoutes: Route[] = [
   {
     path: '',
     providers: [UserService],
-    resolve: { userListResolver },
     component: UserListComponent,
   },
 ];
--- apps/frontend/src/app/views/user-list/user-list.component.ts
+++ apps/frontend/src/app/views/user-list/user-list.component.ts
@@ -1,6 +1,8 @@
 import { JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
+import { injectDispatch } from '@ngrx/signals/events';
+import { userEvents } from '../../state/user.events';
 import { UserStore } from '../../state/user.store';
 
 @Component({
@@ -13,7 +15,12 @@ import { UserStore } from '../../state/user.store';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserListComponent {
+  private readonly dispatch = injectDispatch(userEvents);
   private readonly store = inject(UserStore);
 
   protected userList = this.store.userList;
+
+  constructor() {
+    this.dispatch.loadUserList();
+  }
 }
